364368e9d4e0572f0f8baa709cc5286af8be1dcd
Fahxoxo <fahpemika@gmail.com>
2025-04-06 11:57:56 +0700

    Initial commit

---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+
+Route::get('/', function () {
+    return Inertia::render('welcome');
+})->name('home');
+
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('dashboard', function () {
+        return Inertia::render('dashboard');
+    })->name('dashboard');
+});
+
+require __DIR__.'/settings.php';
+require __DIR__.'/auth.php';
+
+
+Route::get('/test', function () {
+    return Inertia::render('Test');
+})->name('test');
+
+Route::get('/tictactoe', function () {
+    return Inertia::render('Tictactoe');
+})->name('tictactoe');
+
+Route::get('/fruit', function () {
+    return Inertia::render('Fruit');
+})->name('fruit');
